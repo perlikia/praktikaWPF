b838da0e86a61f52af0c3cece2cb41d1da8977db
ivanMihailovPlaceStart <ivan.mihailov@place-start.ru>
2022-12-24 00:08:23 +0300

    first commit

---
+++ EmployerCostCalculate/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployerCostCalculate
+{
+    public class EmployerCost
+    {
+        public static double CalculateByPost(int post_id)
+        {
+            switch (post_id)
+            {
+                case 1:
+                    return 50000;
+            }
+
+            return 0;
+        }
+
+    }
+}
